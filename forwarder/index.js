1961bd3314df3d0da62810dac3c6cdf5b6c7c58b
RFlintstone <rubenflint@hotmail.com>
2023-05-31 07:48:56 +0000

    https support

--- forwarder/index.js
+++ forwarder/index.js
@@ -1,13 +1,32 @@
 require('dotenv').config();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const online_log = require("online-log");
 const bodyParser = require('body-parser');
 const express = require('express');
 const axios = require('axios');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
-// Create our Express instance
+//Create our Express instance
 const app = express();
 const port = process.env.FORWARDER_PORT;
+
+// Specify the paths to the existing SSL certificate and key files
+const options = {
+  key: fs.readFileSync('./cert/country_key.pem'),
+  cert: fs.readFileSync('./cert/out.pem'),
+  ca: [fs.readFileSync('./cert/noob-root.pem'), fs.readFileSync('./cert/country-ca.pem')],
+  requestCert: true,
+  rejectUnauthorized: false
+};
+
+const axi = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
+});
 
 // Parse incoming request bodies in a middleware before your handlers, available under the req.body property
 app.use(bodyParser.urlencoded({extended: false}));
@@ -19,13 +38,47 @@ const {log} = online_log;
 
 // Define the routing table based on client name
 const routingTable = {
-    "noob-server":  "http://127.0.0.1:8082/api/v1/receive-data",
-    "server-1":     "http://server1.com/api/v1/receive-data",
+    "noob-server":  "https://145.24.222.82:8443/api/balance",
+    "exit":         "http://31.151.46.128:8081",
     "server-2":     "http://server2.com/api/v1/receive-data",
     "server-3":     "http://server3.com/api/v1/receive-data",
     "server-4":     "http://server4.com/api/v1/receive-data",
     "dev":          "http://127.0.0.1:8082/api/v1/receive-data"
 };
+
+app.post('/api/balance', async (req, res) => {
+    console.log(req.body);
+    res.send({"accNo":"1234"});
+    //const data = req.body;
+    //
+    //try {
+    //  const response = await axiosInstance.post('https://145.24.222.82:8443/api/balance', data)
+    //  res.send(response.data);
+    //} catch(error) {
+    //  console.error(error);
+    //  res.send('Ballance request not succesfull ' + error);
+    //}
+})
+
+app.get('/register', async (req, res) => {
+    try {
+      const response = await axi.get('https://145.24.222.82:8443/api/register', { httpsAgent: agent });
+      console.log(response.data);
+      res.send(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error retrieving data');
+    }
+})
+
+app.get('/peer-certificate', (req, res) => {
+  const peerCertificate = req.socket.getPeerCertificate();
+  if (peerCertificate) {
+    res.json(peerCertificate);
+  } else {
+    res.status(404).send('Peer certificate not available');
+  }
+});
 
 app.post('/api/v1/route-data', async (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress                                                                   // Get (client) IP
@@ -51,8 +104,11 @@ app.post('/api/v1/route-data', async (req, res) => {
     // Specified name is in the routing table, now lets pass the data we got accordingly
     try {
         const response = await axios.post(routingTable[toBank], req.body);                                                                  // Forward body using POST
+        const data = JSON.stringify(req.body);
+        console.log(data);
+
         if (response.data !== "OK") {                                                                                                       // Make sure the response we got back is OK
-            res.sendStatus(500)                                                                                                        // Send 500 code if the request is not OK
+            res.sendStatus(500);                                                                                                        // Send 500 code if the request is not OK
             return;                                                                                                                         // And stop code
         }
 
@@ -86,6 +142,8 @@ app.all('/api/v1/expected', async (req, res) => {
     res.status(200).json(json)
 })
 
-app.listen(port, () => {
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
